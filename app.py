b0729005c8688b79b877ba2ceb0384101dfc568b
Winyou Singsathorn <winyou.s327@gmail.com>
2026-02-20 09:01:20 +0700

    Added register functionality with password hashing

--- app.py
+++ app.py
@@ -73,11 +73,7 @@ def login():
 
     return render_template("login.html")
 
-@app.route("/logout")
-@login_required
-def logout():
-    logout_user()
-    return redirect(url_for("home"))
+
 
 
 if __name__ == "__main__":
