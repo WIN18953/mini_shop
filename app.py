e150b97a0ec861ac8cb2705108377d7c0b4d21f8
Winyou Singsathorn <winyou.s327@gmail.com>
2026-02-24 21:27:45 +0700

    Initial project setup with database models

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
-from flask import Flask, render_template, request, redirect, url_for, session
-from models import db, User
-from flask_login import LoginManager, login_user, login_required, logout_user, current_user
-from werkzeug.security import generate_password_hash, check_password_hash
+from flask import Flask, render_template
+from models import db, User, Product, Cart
+from flask_login import LoginManager
 
 app = Flask(__name__)
-app.secret_key = "supersecret"
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+app.config['SECRET_KEY'] = 'secret123'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 
 db.init_app(app)
 
@@ -17,41 +16,11 @@ login_manager.login_view = "login"
 def load_user(user_id):
     return User.query.get(int(user_id))
 
+
 @app.route("/")
 def home():
-    if current_user.is_authenticated:
-        return f"Hello {current_user.username} | <a href='/logout'>Logout</a>"
-    return "You are not logged in | <a href='/login'>Login</a>"
+    return "<h1>Mini Shop is Running 🚀</h1>"
 
-@app.route("/register", methods=["GET","POST"])
-def register():
-    if request.method == "POST":
-        hashed_password = generate_password_hash(request.form["password"])
-        new_user = User(
-            username=request.form["username"],
-            email=request.form["email"],
-            password=hashed_password
-        )
-        db.session.add(new_user)
-        db.session.commit()
-        return redirect(url_for("login"))
-    return render_template("register.html")
-
-@app.route("/login", methods=["GET","POST"])
-def login():
-    if request.method == "POST":
-        user = User.query.filter_by(email=request.form["email"]).first()
-        if user and check_password_hash(user.password, request.form["password"]):
-            login_user(user)
-            return redirect(url_for("home"))
-        return "Login Failed"
-    return render_template("login.html")
-
-@app.route("/logout")
-@login_required
-def logout():
-    logout_user()
-    return redirect(url_for("home"))
 
 if __name__ == "__main__":
     with app.app_context():
