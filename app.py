6d05d2030c3d968fb63a8ab607759a5afedea69f
Winyou Singsathorn <winyou.s327@gmail.com>
2026-02-24 20:51:58 +0700

    add login

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+
+        # ทดสอบแบบง่ายก่อน
+        if username == "admin" and password == "1234":
+            return "Login Success"
+        else:
+            return "Login Failed"
+
+    return render_template("login.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
