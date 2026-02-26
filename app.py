abaeb55973b313dd6dd9b307fba6686d7fcf7feb
Winyou Singsathorn <winyou.s327@gmail.com>
2026-02-26 20:09:09 +0700

    Edit app

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 from models import db
 from flask_login import LoginManager
+from models import db, User
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'supersecretkey'
@@ -10,6 +11,10 @@ db.init_app(app)
 
 login_manager = LoginManager()
 login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 @app.route("/")
 def home():
