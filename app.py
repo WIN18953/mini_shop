3ed65db580a09bb5350cf0a1dc5007aff717ddb0
Winyou Singsathorn <winyou.s327@gmail.com>
2026-02-26 20:14:26 +0700

    Add register route

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+from flask import request, redirect, url_for, flash
+from werkzeug.security import generate_password_hash
 from flask import Flask, render_template
 from models import db
 from flask_login import LoginManager
@@ -19,6 +21,21 @@ def load_user(user_id):
 @app.route("/")
 def home():
     return render_template("home.html")
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        username = request.form["username"]
+        email = request.form["email"]
+        password = generate_password_hash(request.form["password"])
+
+        new_user = User(username=username, email=email, password=password)
+        db.session.add(new_user)
+        db.session.commit()
+
+        flash("สมัครสมาชิกสำเร็จ 🎉")
+        return redirect(url_for("home"))
+
+    return render_template("register.html")
 
 if __name__ == "__main__":
     with app.app_context():
