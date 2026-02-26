12e320eca17025cfa022bf30446da2bd10e2f317
Winyou Singsathorn <winyou.s327@gmail.com>
2026-02-26 20:19:52 +0700

    Add login and logout functionality

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+from werkzeug.security import check_password_hash
+from flask_login import login_user, logout_user, login_required, current_user
 from flask import request, redirect, url_for, flash
 from werkzeug.security import generate_password_hash
 from flask import Flask, render_template
@@ -36,6 +38,23 @@ def register():
         return redirect(url_for("home"))
 
     return render_template("register.html")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        email = request.form["email"]
+        password = request.form["password"]
+
+        user = User.query.filter_by(email=email).first()
+
+        if user and check_password_hash(user.password, password):
+            login_user(user)
+            flash("เข้าสู่ระบบสำเร็จ 🎉")
+            return redirect(url_for("home"))
+        else:
+            flash("อีเมลหรือรหัสผ่านไม่ถูกต้อง ❌")
+
+    return render_template("login.html")
 
 if __name__ == "__main__":
     with app.app_context():
